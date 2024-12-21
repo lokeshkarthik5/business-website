869d42af50c9c08c73742c99f720da7365915794
lokeshkarthik5 <lokeshkarthic1999@gmail.com>
2024-12-21 16:58:58 +0530

    home

--- app/page.js
+++ app/page.js
@@ -1,9 +1,10 @@
-import Image from "next/image";
+import PortfolioForm from "@/components/PortfolioForm";
 
 export default function Home() {
   return (
-    <div>
-      Home Page
-    </div>
+    <main className="min-h-screen p-8">
+      <h1 className="text-3xl font-bold mb-8">Portfolio Website Generator</h1>
+      <PortfolioForm />
+    </main>
   );
 }
